2e092bb5000a04568070781fb865abb8a024500b
MaximKucheriaviy <96634366+MaximKucheriaviy@users.noreply.github.com>
2023-12-17 19:41:32 +0200

    adress fix

--- components/Contacts/Contacts.jsx
+++ components/Contacts/Contacts.jsx
@@ -11,9 +11,9 @@ export const Contacts = () => {
         <div className="linkContainer">
           <Link
             className="geo"
-            href="https://maps.app.goo.gl/WEQttbMfBu1XEH4LA"
+            href="https://www.google.com/maps/place/%D0%B2%D1%83%D0%BB%D0%B8%D1%86%D1%8F+%D0%9C%D0%B8%D0%BA%D0%BE%D0%BB%D0%B8+%D0%93%D0%BE%D0%BB%D0%B5%D0%B3%D0%BE,+7%D0%92,+%D0%9A%D0%B8%D1%97%D0%B2,+03058/@50.4386028,30.4349909,17z/data=!3m1!4b1!4m6!3m5!1s0x40d4cc1ea427bced:0x2bf56d9cccc09cf2!8m2!3d50.4385994!4d30.4375712!16s%2Fg%2F11b6b6kw71?entry=ttu"
           >
-            м.Київ вул.Хрещатик 22
+            вул. Миколи Голего 7-В
           </Link>
           <Link className="tel" href="tel:+380978588077">
             тел:+38(097)-858-80-77
