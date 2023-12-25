578a50c2d2e3ff42952e88e7914003c4c2fe772b
Maxim Kucheriaviy <maxim-k@i.ua>
2023-12-25 18:48:01 +0200

    galery fix

--- components/Galery/Galery.jsx
+++ components/Galery/Galery.jsx
@@ -1,62 +1,33 @@
 import { StyledGalerySection } from "./StyledGalery";
 import { Container } from "../Container/Container";
-import Carousel from "react-material-ui-carousel";
-import Image from "next/image";
-import { useState, useEffect } from "react";
 import { useDementions } from "@/service/useDemenshions";
+import Image from "next/image";
+import { ImageList, ImageListItem } from "@mui/material";
 
-export const Galery = ({ splitedGalery }) => {
-  const [galery, setGalery] = useState(null);
-  const dementions = useDementions();
-
-  useEffect(() => {
-    if (!dementions.x) {
-      return;
-    }
-    if (dementions.x < 768) {
-      setGalery(splitedGalery());
-    } else {
-      setGalery(splitedGalery(2));
-    }
-  }, [dementions, splitedGalery]);
-
+export const Galery = ({ galery = [], nameShow = true }) => {
   return (
     <StyledGalerySection id="galery">
       <Container>
-        <h2>Галерея</h2>
-        <Carousel
-          className="carousel"
-          navButtonsAlwaysInvisible={dementions.x && dementions.x < 768}
-        >
-          {galery &&
-            galery.map((item, index) => {
-              return (
-                <div key={index} className="galeryItem">
-                  {item.map((item, index) => {
-                    return (
-                      <div key={index} className="imageThumb">
-                        {item.name.includes("vert") ? (
-                          <Image
-                            src={item.name}
-                            alt="galery"
-                            width={496}
-                            height={496}
-                          />
-                        ) : (
-                          <Image
-                            src={item.name}
-                            alt="galery"
-                            width={279}
-                            height={496}
-                          />
-                        )}
-                      </div>
-                    );
-                  })}
-                </div>
-              );
-            })}
-        </Carousel>
+        {nameShow && <h2>Галерея</h2>}
+        <ImageList cols={4} variant="quilted" rowHeight={248} gap={20}>
+          {galery.map((item) => {
+            return (
+              <ImageListItem
+                className="imageItem"
+                key={item.name}
+                cols={item.cols || 1}
+                rows={item.rows || 2}
+              >
+                <Image
+                  src={item.name}
+                  alt="galeryImage"
+                  width={item.name.includes("vert") ? 297 : 496}
+                  height={496}
+                />
+              </ImageListItem>
+            );
+          })}
+        </ImageList>
       </Container>
     </StyledGalerySection>
   );
--- service/galery.js
+++ service/galery.js
@@ -1,12 +1,10 @@
-const galery = [
-  {
-    name: "/galery/main/g1vert.jpg",
-  },
+export const galery = [
   {
     name: "/galery/main/g2.jpg",
+    cols: 2,
   },
   {
-    name: "/galery/main/g3.jpg",
+    name: "/galery/main/g1vert.jpg",
   },
   {
     name: "/galery/main/g4vert.jpg",
@@ -15,10 +13,16 @@ const galery = [
     name: "/galery/main/g5vert.jpg",
   },
   {
+    name: "/galery/main/g3.jpg",
+    cols: 2,
+  },
+  {
     name: "/galery/main/g6.jpg",
+    cols: 2,
   },
   {
     name: "/galery/main/g7.jpg",
+    cols: 2,
   },
 ];
 
